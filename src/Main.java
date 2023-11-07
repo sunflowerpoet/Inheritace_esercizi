f6554ff7cffc9c9e8fdd1cd830dd60d320076712
sunflowerpoet <anna.simionato747@gmail.com>
2023-11-07 15:46:08 +0100

    first commit inheritance

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*Scrivere un programma che contenga una classe chiamata Animale ed un metodo chiamato faiIlVerso()
+che stampi il verso dell'animale.
+Crea poi una sottoclasse chiamata Gatto che fa override del metodo faiIlVerso() per miagolare.
+Prova quindi a far stampare il verso di
+Animale e di Gatto.*/
+
+public class Main {
+    public static void main(String[] args) {
+        Animale animale = new Animale();
+        Gatto gatto = new Gatto();
+
+        animale.faiIlVerso();
+        System.out.println("Il verso del gatto e':");
+        gatto.faiIlVerso();
+    }
+}
